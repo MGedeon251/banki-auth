ae10a9642c1c9a5d0a51fc4b777755a462a4000c
Mardochet G. LEKOUNDA <gedeon.lekounda@gmail.com>
2023-02-19 00:07:05 +0100

    actualisation auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
-  }
+@UseGuards(LocalAuthGuard)
+@Post('auth/login')
+async login(@Request() req) {
+  return req.user;
 }
+
+}
+
 
